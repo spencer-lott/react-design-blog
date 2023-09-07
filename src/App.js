93ffd9cb9fea41dcb36e1868b7b6d04daa59f50b
Spencer Lott <spklott@gmail.com>
2023-09-06 20:29:55 -0400

    initializing

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./navbar/Navbar";
+import Home from "./pages/home/Home";
+
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <Navbar />
+        <Home />
+      </>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import "./Navbar.css"
+
+export default function Navbar() {
+  return (
+    <div className='nav'>
+        <div className="topLeft">
+            <i className="topIcon fa-brands fa-square-facebook"></i>
+            <i className="topIcon fa-brands fa-square-twitter"></i>
+            <i className="topIcon fa-brands fa-square-pinterest"></i>
+            <i className="topIcon fa-brands fa-square-instagram"></i>
+        </div>
+        <div className="topCenter">
+            <ul className="topList">
+                <li className="topListItem">HOME</li>
+                <li className="topListItem">ABOUT</li>
+                <li className="topListItem">CONTACT</li>
+                <li className="topListItem">WRITE</li>
+                <li className="topListItem">LOGOUT</li>
+            </ul>
+        </div>
+        <div className="topRight">
+            <img 
+            className="topImg"
+            src="https://img-cdn.tid.al/o/533f0a478768940b012e685249d015dca20f4820.jpg" alt="" />
+            <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+        </div>
+
+      
+    </div>
+  )
+}
